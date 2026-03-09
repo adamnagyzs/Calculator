772cda2da0b3200fb3f6ddd792fb8331a138ba4e
Microwave2000 <nagyzsadam@gmail.com>
2026-03-09 17:45:17 +0100

    number buttons, operator buttons, equal button works

---
+++ script.js
@@ -0,0 +1,71 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+resultContainer = document.getElementById("resultContainer");
+
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let currentElement = "";
+
+const numberButtons = document.querySelectorAll(".numberButton");
+
+const buttonClear = document.getElementById("buttonClear");
+buttonClear.addEventListener("click", () => {
+  firstNum = "";
+  secondNum = "";
+  operator = "";
+  resultContainer.textContent = "";
+});
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (!operator) {
+      firstNum += button.textContent;
+      currentElement = firstNum;
+    } else {
+      secondNum += button.textContent;
+      currentElement = secondNum;
+    }
+    resultContainer.textContent = `${firstNum} ${operator} ${secondNum}`;
+  });
+});
+
+const operatorButtons = document.querySelectorAll(".operatorButton");
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    resultContainer.textContent = firstNum;
+    operator = button.textContent;
+    resultContainer.textContent += ` ${operator} `;
+    currentElement = operator;
+  });
+});
+
+const operations = {
+  "+": add,
+  "-": subtract,
+  x: multiply,
+  "÷": divide,
+};
+
+const equalButton = document.getElementById("equalButton");
+equalButton.addEventListener("click", () => {
+  firstNum = operations[operator](Number(firstNum), Number(secondNum));
+  secondNum = "";
+  operator = "";
+  resultContainer.textContent = firstNum;
+});
